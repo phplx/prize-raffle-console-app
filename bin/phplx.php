4e1bc29ff3786237ee1e0d3d20c49776226d752a
Daniel Gomes <me@danielcsgomes.com>
2013-10-06 16:18:39 +0100

    Added missing bin files; Removed bin from .gitignore

---
+++ bin/phplx.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the phplx Prize Raffle Console Application package.
+ *
+ * (c) 2013 phplx.net
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Phplx\Application;
+use Phplx\Command\FetchAttendeesCommand;
+use Phplx\Command\ListPrizesCommand;
+use Phplx\Command\ListWinnersCommand;
+use Phplx\Command\LoadPrizesCommand;
+use Phplx\Command\RaffleCommand;
+use Phplx\Command\TweetCommand;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+$configDirectories = array(
+    __DIR__ . '/../config',
+    __DIR__ . '/../cache'
+);
+
+$container = include __DIR__ . '/../src/container.php';
+
+$loader = new YamlFileLoader($container, new FileLocator($configDirectories));
+$loader->load('parameters.yaml');
+
+$fetchAttendeesCmd = new FetchAttendeesCommand();
+$loadPrizesCmd = new LoadPrizesCommand();
+$raffleCmd = new RaffleCommand();
+$listPrizesCmd = new ListPrizesCommand();
+$listWinnersCmd = new ListWinnersCommand();
+$sendTweetCmd = new TweetCommand();
+
+$app = new Application();
+$app->setContainer($container);
+
+$app->addCommands(
+    array(
+         $fetchAttendeesCmd,
+         $loadPrizesCmd,
+         $listPrizesCmd,
+         $listWinnersCmd,
+         $raffleCmd,
+         $sendTweetCmd
+    )
+);
+$app->run();
